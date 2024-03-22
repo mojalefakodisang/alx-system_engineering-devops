dd0ef7fa6457f4d6b1a373429a363472d7ca6041
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-22 22:45:09 +0200

    [Update] 0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# create a file in /tmp using puppet
+# creates a file in /tmp using puppet
 file {'/tmp/school':
-    mode	=> '0744',
-    owner	=> 'www-data',
-    group	=> 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet'
 }
+
