c058c52503a7af7617a6d3ecb77c21a0dde4231c
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-24 20:02:57 +0200

    [Update] - Config SSH for no password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 
 $line_string = "Host 34.239.250.176\n\
     IndentityFile ~/.ssh/school\n\
-    PasswordAuthentication no\n\"
+    PasswordAuthentication no\n"
 
 file {"/etc/ssh/ssh_config":
     ensure => file,
