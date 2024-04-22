ae5672f677c9ddef2b854087de96e10b497b982c
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-04-23 01:33:01 +0200

    [Update] - CSV file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,22 +10,14 @@ if __name__ == '__main__':
 
     user_id = sys.argv[1]
     basename = "https://jsonplaceholder.typicode.com/"
-    user_data = requests.get(basename + "users")
-    todos_data = requests.get(basename + "todos")
+    user_data = requests.get(basename + "users/" + user_id).json()
+    todos_data = requests.get(basename + "todos",
+                              params={"userId": user_id}).json()
 
-    for user in user_data.json():
-        if user_id == str(user['id']):
-            name = user['name']
-            t_tasks = []
-            for task in todos_data.json():
-                title = task['title']
-                userId = str(task['userId'])
-                completed = task['completed']
-                if userId == user_id:
-                    name = user['name']
-                    t_tasks.append([user_id, name, completed, title])
-
-    with open(f"{user_id}.csv", "w") as f:
-        writer = csv.writer(f)
-        for x in t_tasks:
-            writer.writerow(x)
+    c_file = f"{user_id}.csv"
+    with open(c_file, "w") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todos_data:
+            slctd = [task.get(key, 'N/A') for key in ['completed', 'title']]
+            line = [user_data.get('id'), user_data.get('username')] + slctd
+            writer.writerow(line)
