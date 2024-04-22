4f3cd61712d92b886183af3900313650180b65ef
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-04-23 00:20:41 +0200

    [Update] - Get user tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module that returns tasks of an employee
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    user_id = int(sys.argv[1])
+    basename = "https://jsonplaceholder.typicode.com/"
+    user_data = requests.get(basename + "users")
+    todos_data = requests.get(basename + "todos")
+
+    for user in user_data.json():
+        if user['id'] == user_id:
+            name = user['name']
+        l_tasks = []
+        count = total = 0
+        for task in todos_data.json():
+            title = task['title']
+            userId = task['userId']
+            completed = task['completed']
+            if user_id == userId:
+                if completed:
+                    count += 1
+                    l_tasks.append(title)
+                total += 1
+
+    print(f"Employee {name} is done with tasks({count}/{total}):")
+    for x in l_tasks:
+        print(f"\t {x}")
