624ce0ec0f4f5ffe244ae7f22ffb22dd8fbd865a
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-06-18 21:33:36 +0200

    [Update] - Altered with 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,19 +4,19 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Queries a Reddit subreddit and return number of subscribers"""
+base_url = "https://www.reddit.com"
 
-    base_url = "https://www.reddit.com"
-    end_point = "/subreddits/search"
-
-    headers = {
+headers = {
             'Accept': 'application/json',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
                     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 \
                     Safari/537.36'
-    }
-    res = requests.get("{}/r/{}/about/.json".format(base_url, subreddit),
+}
+
+
+def number_of_subscribers(subreddit):
+    """Queries a Reddit subreddit and return number of subscribers"""
+    res = requests.get("{}/r/{}/about.json".format(base_url, subreddit),
                        headers=headers, allow_redirects=False)
 
     if res.status_code == 200 and 'subscribers' in res.json()['data']:
