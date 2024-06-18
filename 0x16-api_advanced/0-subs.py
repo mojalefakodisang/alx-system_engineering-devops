2eba86e407807a03dcf2f56e72507c5362836096
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-06-18 14:58:51 +0200

    [Update] - Queries subscribers of a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that queries the Reddit API"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """Queries a Reddit subreddit and return number of subscribers"""
+    if subreddit == None or len(subreddit) < 1 \
+    or not isinstance(subreddit, str) == True:
+        return 0
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        d_json = response.json()
+        return d_json['data']['subscribers']
+    else:
+        return 0
