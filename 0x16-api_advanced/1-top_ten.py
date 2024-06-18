338be14aa2b967fd04b4276e6e7f620e06fd01ec
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-06-18 21:22:48 +0200

    [Update] - Fixed Top 10 hot titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,24 +3,27 @@
 
 import requests
 
+base_url = "https://www.reddit.com"
 
-def top_ten(subreddit):
-    """Queries a Reddit subreddit and return number of subscribers"""
-
-    base_url = "https://www.reddit.com"
-
-    headers = {
+headers = {
             'Accept': 'application/json',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
                     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 \
                     Safari/537.36'
-    }
+}
+
+
+def top_ten(subreddit):
+    """Queries a Reddit subreddit and return number of subscribers"""
     res = requests.get("{}/r/{}/.json?\
                        sort=top&limit=10".format(base_url, subreddit),
                        headers=headers, allow_redirects=False)
 
     if res.status_code == 200:
-        for post in res.json()['data']['children'][0:10]:
+        data = res.json().get('data', {})
+        children = data.get('children', {})
+
+        for post in children[0:10]:
             print(post['data']['title'])
     else:
         print(None)
