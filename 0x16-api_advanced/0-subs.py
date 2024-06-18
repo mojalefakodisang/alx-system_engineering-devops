94bbd2878fcb7a37830d5775274dd01def245996
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-06-18 15:30:24 +0200

    [Update] - Changed file mod

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """Module that queries the Reddit API"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Queries a Reddit subreddit and return number of subscribers"""
+
     base_url = "https://www.reddit.com"
     end_point = "/subreddits/search"
 
     headers = {
             'Accept': 'application/json',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-                    AppleWebkit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 \
+                    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 \
                     Safari/537.36'
     }
-    url = "{}/r/{}/about/.json".format(base_url, subreddit)
+    res = requests.get("{}/r/{}/about/.json".format(base_url, subreddit),
+                       headers=headers, allow_redirects=False)
 
-    res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200 and 'subscribers' in res.json()['data']:
         return res.json()['data']['subscribers']
     else:
